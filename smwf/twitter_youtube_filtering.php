291d1f0bdc259ea7cd6330cdad86f4355836c9ea
Scott Hurley <scott.hurley@stfc.ac.uk>
2022-04-25 09:28:56 +0100

    var names changes, cron methods added, bugs fixed, files for twitter and youtube ids added

--- smwf/twitter_youtube_filtering.php
+++ smwf/twitter_youtube_filtering.php
@@ -7,8 +7,6 @@ if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
   throw new Exception(sprintf('Please run "composer require google/apiclient:~2.0" in "%s"', __DIR__));
 }
 
-
-
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 //TWITTER SETUP
@@ -19,38 +17,156 @@ $access_token_secret = '';
 $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
 $connection->setApiVersion('2');
-$userIdResponse = $connection->get('users/by', ['usernames' => 'STFC_Matters']);
-$userIdLiteral = $userIdResponse->data[0]->id;
-$userNames = readJsonFile('twitter_usernames.json');
-$twitterKeywords = array('update', 'learn');
-$twitterMaxResults = '20';
+$user_names = get_object_from_file('twitter_usernames');
+$user_ids = get_object_from_file('twitter_user_ids');
+$twitter_keywords = array('update', 'learn');
+$twitter_max_results = '20';
 //TWITTER SETUP
 
 //YOUTUBE SETUP
-$developerKey = '';
+$developer_key = '';
 $client = new Google_Client();
 $client->setApplicationName('API code samples');
-$client->setDeveloperKey($developerKey);
+$client->setDeveloperKey($developer_key);
 
 // Define service object for making API requests.
 $service = new Google_Service_YouTube($client);
 
-$youtubeKeywords = ['matter', 'daresbury'];
-$channelIds = readJsonFile('youtube_channel_ids.json');
-$youtubeMaxResults = 10;
+$youtube_keywords = ['matter', 'daresbury'];
+$channel_ids = get_object_from_file('youtube_channel_ids');
+$youtube_max_results = 10;
 //YOUTUBE SETUP
+
+//TO BE CALLED BY OTHER STUFF
+
+//Initialises or updates the variables and files for all user's tweets
+function update_tweets() {
+	global $user_names, $user_ids;
+	
+	foreach ($user_names as &$user_name) {
+		global ${$user_name . '_tweets'};
+		
+		$tweets_from_file = get_object_from_file("$user_name" . "_tweets");
+		
+		//If the tweets are in memory, use that
+		if(isset(${$user_name . '_tweets'})) {
+			$latest_id = ${$user_name . '_tweets'}[0]->id;
+			
+			try {
+				$new_tweets = get_new_tweets($user_ids[$user_name], $latest_id);
+				${$user_name . '_tweets'} = array_merge($new_tweets, ${$user_name . '_tweets'});
+				write_object_to_file(${$user_name . '_tweets'}, "$user_name" . "_tweets");
+			} catch(Exception $e) {
+				echo 'Message: ' .$e->getMessage();
+			}
+		//Else use the data loaded from file if it exists
+		} elseif(!is_null($tweets_from_file)) {
+			$latest_id = $tweets_from_file[0]->id;
+			
+			try {
+				$new_tweets = get_new_tweets($user_ids[$user_name], $latest_id);
+				${$user_name . '_tweets'} = array_merge($new_tweets, $tweets_from_file);
+				write_object_to_file(${$user_name . '_tweets'}, "$user_name" . "_tweets");
+			} catch(Exception $e) {
+				echo 'Message: ' .$e->getMessage();
+			}
+		//Clean initialisation
+		} else {
+			try {
+				$collected_tweets = collect_tweets($user_ids[$user_name]);
+			
+				if(count($collected_tweets) > 0) {
+					${$user_name . '_tweets'} = $collected_tweets;
+					write_object_to_file(${$user_name . '_tweets'}, "$user_name" . "_tweets");
+				}
+			} catch(Exception $e) {
+				echo 'Message: ' .$e->getMessage();
+			}
+		}
+	}
+}
+
+//Initialises or updates the variables and files for all channels's youtube videos
+function update_youtube_videos() {
+	global $channel_ids;
+	
+	foreach ($channel_ids as &$channel_id) {
+		global ${$channel_id . '_videos'};
+		$videos_from_file = get_object_from_file("$channel_id" . "_videos");
+		
+		//If the videos are in memory, use that
+		if(isset(${$channel_id . '_videos'})){
+			$video_list = ${$channel_id . '_videos'};
+		//Else use the data loaded from file if it exists
+		} elseif(!is_null($videos_from_file)) {
+			$video_list = $videos_from_file;
+		//Clean initialisation
+		} else {
+			$video_list = null;
+		}
+		
+		try {
+			${$channel_id . '_videos'} = get_channel_videos($channel_id, $video_list);
+			write_object_to_file(${$channel_id . '_videos'}, "$channel_id" . "_videos");
+		} catch(Exception $e) {
+			echo 'Message: ' .$e->getMessage();
+		}
+	}
+}
+
+update_youtube_videos();
+
+//Called by cron for updating/initilising social medias
+function update_social_medias() {
+	update_tweets();
+	update_youtube_videos();
+}
+
+//on page load assign keyword array relevant to page to a global var for the filter function?
+//get the keywords from Option[$user_name]?
+//To be called externally for filtering, uses a file as a fallback
+function filter_tweets($user_name) {
+	if(isset(${$user_name . '_tweets'})) {
+		return array_filter(${$user_name . '_tweets'}, 'tweet_contains_keyword');
+	}
+	
+	$tweets_from_file = get_object_from_file("$user_name" . "_tweets");
+	
+	if(!is_null($tweets_from_file)) {
+		${$user_name . '_tweets'} = $tweets_from_file;
+		return array_filter(${$user_name . '_tweets'}, 'tweet_contains_keyword');
+	}
+	
+	return array();
+}
+
+//To be called externally for filtering, uses a file as a fallback
+function filter_videos($channel_id) {
+	if(isset(${$channel_id . '_videos'})) {
+		return array_filter(${$channel_id . '_videos'}->items, 'video_contains_keyword');
+	}
+	
+	$videos_from_file = get_object_from_file("$channel_id" . "_videos");
+	
+	if(!is_null($videos_from_file)) {
+		${$channel_id . '_videos'} = $videos_from_file;
+		return array_filter(${$channel_id . '_videos'}->items, 'video_contains_keyword');
+	}
+	
+	return array();
+}
+
+//TO BE CALLED BY OTHER STUFF
 
 //TWITTER STUFF
 
-//limit is here just for testing purposes
-//if this function returns an empty array, don't update the tweets lists
-function collect_tweets($userId) {
-	global $connection, $twitterMaxResults;
+//Fetches as many of a user's tweets as the API allows
+function collect_tweets($user_id) {
+	global $connection, $twitter_max_results;
 	
 	$tweets = array();
-	$limit = 0;
 	
-	$result = $connection->get("users/$userId/tweets", ['max_results' => $twitterMaxResults]);
+	$result = $connection->get("users/$user_id/tweets", ['max_results' => $twitter_max_results]);
 	
 	if(is_null($result) || property_exists($result, 'errors')) {
 		return array();
@@ -58,29 +174,26 @@ function collect_tweets($userId) {
 	
 	$tweets = array_merge($tweets, $result->data);
 	
+	//while there are still tweets to get, fetch them and add them to the list
 	while(property_exists($result->meta, 'next_token')) {
-		$result = $connection->get("users/$userId/tweets", ['max_results' => $twitterMaxResults, 'pagination_token' => $result->meta->next_token]);
+		$result = $connection->get("users/$user_id/tweets", ['max_results' => $twitter_max_results, 'pagination_token' => $result->meta->next_token]);
 		
 		if(is_null($result) || property_exists($result, 'errors')) {
 			return array();
 		}
 	
 		$tweets = array_merge($tweets, $result->data);
-		
-		if ($limit == 0) {
-			break;
-		}
-		
-		$limit++;
 	}
 	
 	return $tweets;
 }
 
-// use array_filter(collect_tweets(), 'tweetContainsKeyword')
-function tweetContainsKeyword($tweet) {
-	global $twitterKeywords;
-	foreach ($twitterKeywords as &$keyword) {
+//Returns true if a tweet contains a keyword, otherwise false
+//Use array_filter() e.g. array_filter($tweets, 'tweet_contains_keyword')
+function tweet_contains_keyword($tweet) {
+	global $twitter_keywords;
+	
+	foreach ($twitter_keywords as &$keyword) {
 		if(stripos($tweet->text, $keyword) !== false) {
 			return true;
 		}
@@ -89,80 +202,103 @@ function tweetContainsKeyword($tweet) {
 	return false;
 }
 
-//if this function returns an empty array, don't update the tweets lists
-function getNewTweets($userId, $lastTweetId) {
-	global $connection, $twitterMaxResults;
+//Gets a user's new tweets published since the ID of the tweet provided
+function get_new_tweets($user_id, $last_tweet_id) {
+	global $connection, $twitter_max_results;
 	
-	$result = $connection->get("users/$userId/tweets", ['max_results' => $twitterMaxResults, 'since_id' => $lastTweetId]);
+	$tweets = array();
+	$result = $connection->get("users/$user_id/tweets", ['max_results' => $twitter_max_results, 'since_id' => $last_tweet_id]);
 	
-	if(is_null($result) || property_exists($result, 'errors')) {
+	if(is_null($result) || property_exists($result, 'errors') || !property_exists($result, 'data')) {
 		return array();
 	}
 	
-	return $result->data;
+	$tweets = array_merge($tweets, $result->data);
+	
+	//while there are still tweets to get, fetch them and add them to the list
+	while(property_exists($result->meta, 'next_token')) {
+		$result = $connection->get("users/$user_id/tweets", ['max_results' => $twitter_max_results, 'since_id' => $last_tweet_id, 'pagination_token' => $result->meta->next_token]);
+		
+		if(is_null($result) || property_exists($result, 'errors')) {
+			return array();
+		}
+	
+		$tweets = array_merge($tweets, $result->data);
+	}
+	
+	return $tweets;
 }
 
-//if this fails, use a file as backup
-function getTwitterUserIds() {
-	global $connection, $userNames;
+//Gets a list of Twitter IDs from a list of user names
+function get_twitter_user_ids() {
+	global $connection, $user_names;
 	
-	$userIds = array();
-	$userIdResponseArray = $connection->get('users/by', ['usernames' => $userNames]);
+	$user_ids = array();
+	$user_id_response_array = $connection->get('users/by', ['usernames' => $user_names]);
 	
-	if(is_null($userIdResponseArray) || property_exists($userIdResponseArray, 'errors')) {
+	if(is_null($user_id_response_array) || property_exists($user_id_response_array, 'errors')) {
 		return array();
 	}
 	
-	foreach ($userIdResponseArray->data as &$userIdResponse) {
-		$userIds[$userIdResponse->username] = $userIdResponse->id;
+	foreach ($user_id_response_array->data as &$user_id_response) {
+		$user_ids[$user_id_response->username] = $user_id_response->id;
 	}
 	
-	return $userIds;
+	return $user_ids;
 }
-
-print_r(array_filter(collect_tweets(getTwitterUserIds()['STFC_Matters']), 'tweetContainsKeyword'));
 
 //TWITTER STUFF
 
 //YOUTUBE_STUFF
 
-//functions that make a failed youtube api call will throw an exception
-function getChannelVideos($channelId, $currentVideoList = null) {	
-	global $youtubeMaxResults, $service;
+//Gets either all of a YouTube channel's videos or just the new ones not already saved
+//The number of new videos to fetch is calculated by the new total number of ideos - old total number of videos
+function get_channel_videos($channel_id, $current_video_list = null) {
+	global $youtube_max_results, $service;
 	
-	$previousTotalVideos = is_null($currentVideoList) ? 0 : $currentVideoList->pageInfo->totalResults;
+	$previous_total_videos = is_null($current_video_list) ? 0 : $current_video_list->pageInfo->totalResults;
 	
-	$queryParams = [
-		'channelId' => $channelId,
-		'maxResults' => $youtubeMaxResults,
+	$query_params = [
+		'channelId' => $channel_id,
+		'maxResults' => $youtube_max_results,
 		'order' => 'date',
 		'type' => 'video',
 	];
 	
-	$response = $service->search->listSearch('snippet', $queryParams);
+	$response = $service->search->listSearch('snippet', $query_params);
+	$new_video_list = is_null($current_video_list) ? $response : $current_video_list;
+	$new_total_videos = $response->pageInfo->totalResults;
+	$remaining_videos_to_fetch = $new_total_videos - $previous_total_videos;
 	
-	$newVideoList = is_null($currentVideoList) ? $response : $currentVideoList;
-	$newTotalVideos = $response->pageInfo->totalResults;
-	$remainingVideosToFetch = $newTotalVideos - $previousTotalVideos;
-	$remainingVideosToFetch = 2;
-	$newVideos = array();
-	
-	while(property_exists($response, 'nextPageToken') && $remainingVideosToFetch > 0) {
-		$queryParams['pageToken'] = $response->nextPageToken;
-		$response = $service->search->listSearch('snippet', $queryParams);
+	if($remaining_videos_to_fetch > 0 && $current_video_list != null) {
+		$new_video_list->pageInfo->totalResults = $new_total_videos->pageInfo->totalResults;
+		$new_video_list->items = $remaining_videos_to_fetch < $youtube_max_results ? array_merge($new_video_list->items, array_slice($response->items, 0, $remaining_videos_to_fetch, true)) : array_merge($new_video_list->items, $response->items);
 		
-		$newVideoList->items = $remainingVideosToFetch < $youtubeMaxResults ? array_merge($newVideoList->items, array_slice($response->items, 0, $remainingVideosToFetch, true)) : array_merge($newVideoList->items, $response->items);
-		
-		$remainingVideosToFetch = $remainingVideosToFetch - $youtubeMaxResults;
+		$remaining_videos_to_fetch = $remaining_videos_to_fetch - $youtube_max_results;
 	}
 	
-	return $newVideoList;
+	//while there are more videos to fetch
+	while(property_exists($response, 'nextPageToken') && $remaining_videos_to_fetch > 0) {
+		$query_params['pageToken'] = $response->nextPageToken;
+		$response = $service->search->listSearch('snippet', $query_params);
+		
+		//if we've reached the last page of results we need, only add the remaining number of videos we need, otherwise add the whole page
+		$new_video_list->items = $remaining_videos_to_fetch < $youtube_max_results ? array_merge($new_video_list->items, array_slice($response->items, 0, $remaining_videos_to_fetch, true)) : array_merge($new_video_list->items, $response->items);
+		
+		$remaining_videos_to_fetch = $remaining_videos_to_fetch - $youtube_max_results;
+	}
+	
+	return $new_video_list;
 }
 
-function videoContainsKeyword($video) {
-	global $youtubeKeywords;
+
+//Returns true if a video's title or description contains a keyword, false otherwise
+//Is case insensitive
+//Use with array_filter() e.g. array_filter(get_channel_videos(channel_id, 'video_contains_keyword');
+function video_contains_keyword($video) {
+	global $youtube_keywords;
 	
-	foreach ($youtubeKeywords as &$keyword) {
+	foreach ($youtube_keywords as &$keyword) {
 		if(stripos($video->snippet->title, $keyword) !== false || stripos($video->snippet->description, $keyword) !== false) {
 			return true;
 		}
@@ -171,19 +307,25 @@ function videoContainsKeyword($video) {
 	return false;
 }
 
-print_r(array_filter(getChannelVideos($channelIds[0])->items, 'videoContainsKeyword'));
-
 //YOUTUBE STUFF
 
-function writeJsonFile($object, $fileName) {
-	$fp = fopen($fileName, 'w');
-	fwrite($fp, json_encode($object));
+//Serialises an object and writes it to a file
+//If the file already exists it is overwritten
+function write_object_to_file($object, $file_name) {
+	$fp = fopen($file_name, 'w');
+	fwrite($fp, serialize($object));
 	fclose($fp);
 }
 
-//could add .json extension here instead of having to specify it in arg
-function readJsonFile($fileNanme) {
-	$tweetFileContents = file_get_contents($fileNanme);
+
+//Reads a file and returns an object if possible
+//Returns null if an error is thrown while reading the file
+function get_object_from_file($fileNanme) {
+	try {
+		$file_contents = file_get_contents($fileNanme);
+	} catch(Exception $e) {
+		return null;
+	}
 	
-	return json_decode($tweetFileContents, true);
+	return !$file_contents ? null : unserialize($file_contents);
 }
